d074a627d9d8b6b327402ac0069fa3f5b96b75d5
Unknown <rafagallo7@gmail.com>
2020-08-27 21:35:11 -0300

    Paralax effect coded

---
+++ Assets/Scripts/Game/ParalaxMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParalaxMovement : MonoBehaviour {
+
+    public Transform referenceTransform;
+    public float movementMultiplier;
+
+    private Vector3 ownOrigin;
+    private Vector3 refOrigin;
+
+    private void Start() {
+        ownOrigin = transform.position;
+        refOrigin = referenceTransform.position;
+    }
+    private void Update() {
+        transform.position = ownOrigin + (referenceTransform.position - refOrigin) * movementMultiplier;
+    }
+
+}
