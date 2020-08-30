cd18ebc2f5ce48bf2e03495b159e74a76d1c0beb
helomdrs <heloisam.silva@hotmail.com>
2020-08-30 19:47:56 -0300

    splash images

---
+++ Assets/Scripts/Opening.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Opening : MonoBehaviour
+{
+    public Image img1, img2;
+    public GameObject img1Obj, img2Obj;
+    public string nextScene;
+
+    void Start()
+    {
+        StartCoroutine(FadeImage(img1, true));
+        img2Obj.SetActive(false);
+
+        Invoke("CallLoadScene", 4.5f);
+    }
+
+    void Update()
+    {
+        if(img1.color.a <= 0.1f)
+        {
+            img2Obj.SetActive(true);
+            StartCoroutine(FadeImage(img2, false));
+        } 
+    }
+
+    void CallLoadScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    IEnumerator FadeImage(Image _img, bool fadeAway)
+    {
+        if (fadeAway == true)
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime / 2)
+            {
+                _img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+
+        if (fadeAway == false)
+        {
+            for (float i = 0; i <= 1; i += Time.deltaTime / 2)
+            {
+                _img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+    }
+}
